7592f820e427679e8fcb9e7bad490b0328e3c928
Camron Schwoegler <camron@metafour.org>
2013-10-08 19:40:52 -0600

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Camron Schwoegler on 10/8/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
